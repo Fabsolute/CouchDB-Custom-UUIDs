5e12c040eac31c7b7407ff4996f37fce98c20d9a
Ahmet Türk <ahmetturk93@gmail.com>
2018-06-05 19:30:33 +0300

    make.sh

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 git pull &&
 rm -rf couch_custom_uuids-1.0.0-18-1.6.1 couch_custom_uuids-1.0.0-18-1.6.1.tar.gz &&
 make plugin &&
-rm -rf /usr/lib/x86_64-linux-gnu/couchdb/erlang/lib/couch_custom_uuids-1.0.0-18-1.6.1/ &&
-cp -rf couch_custom_uuids-1.0.0-18-1.6.1 /usr/lib/x86_64-linux-gnu/couchdb/erlang/lib &&
-service couchdb restart &&
-service couchdb status
+sudo rm -rf /usr/lib/x86_64-linux-gnu/couchdb/erlang/lib/couch_custom_uuids-1.0.0-18-1.6.1/ &&
+sudo cp -rf couch_custom_uuids-1.0.0-18-1.6.1 /usr/lib/x86_64-linux-gnu/couchdb/erlang/lib &&
+sudo service couchdb restart &&
+sudo service couchdb status
